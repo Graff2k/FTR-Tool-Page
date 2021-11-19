019c5f8558c990ab6deede9b58f62a05d5b7773c
Louie VanderMolen <lv9437@att.com>
2021-11-19 02:11:18 -0600

    Added Ein P1|P2 function to webpage

---
+++ resources/EIN/p1.js
@@ -0,0 +1,29 @@
+function copyItems() {
+
+    var ipsum = document.getElementById('IpSum');    
+    var trshot = document.getElementById('TrShot');    
+    var stime = document.getElementById('STime'); 
+    var etime = document.getElementById('ETime'); 
+    var dtime = document.getElementById('DTime');
+    var cimp = document.getElementById('CImp');
+    var bridge = document.getElementById('bridge');
+    var output = document.getElementById("output");
+    output.value = "Impact Summary: "+ ipsum.value + "\n"+"Troubleshooting Summary:"+ trshot.value +"\n"
+    +"Start Time: "+ stime.value +"\n"
+    +"End Time: "+ etime.value+ "\n"+
+    "Duration: "+ dtime.value +"\n"
+    + "Estimated Time: "+ cimp.value+"\n" 
+    + "Bridge Information: " + bridge.value
+    
+    output.select();
+    document.execCommand("copy");
+    alert('Please use Ctrl+C to copy')
+    
+    
+}
+
+
+
+
+
+
