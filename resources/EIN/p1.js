887223d782fdac97f49cc680db7e94b48ca8558a
VANDER MOLEN <lv9437@att.com>
2022-08-03 06:13:23 -0500

    Updating website and query

--- resources/EIN/p1.js
+++ resources/EIN/p1.js
@@ -1,19 +1,21 @@
 function copyItems() {
 
     var ipsum = document.getElementById('IpSum');    
-    var trshot = document.getElementById('TrShot');    
+    var trshot = document.getElementById('TrShot');
+    var resol = document.getElementById("Resol");    
     var stime = document.getElementById('STime'); 
     var etime = document.getElementById('ETime'); 
     var dtime = document.getElementById('DTime');
     var cimp = document.getElementById('CImp');
     var bridge = document.getElementById('bridge');
     var output = document.getElementById("output");
-    output.value = "Impact Summary: "+ ipsum.value + "\n"+"Troubleshooting Summary:"+ trshot.value +"\n"
-    +"Start Time: "+ stime.value +"\n"
+    output.value = "Impact Summary: "+ ipsum.value + "\n"+"Troubleshooting Summary: "+ trshot.value +"\n" 
+    +"Resolution: " + resol.value +"\n"
+    +"Start Time: " + stime.value +"\n"
     +"End Time: "+ etime.value+ "\n"+
     "Duration: "+ dtime.value +"\n"
     + "Estimated Customer Impact: "+ cimp.value+"\n" 
-    + "Bridge Information: " + bridge.value
+    + "Groups Engaged: " + bridge.value
     
     output.select();
     document.execCommand("copy");
