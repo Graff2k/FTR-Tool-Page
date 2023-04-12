72243ed6aa42b7304c8ceedcee8bbec48ec4b5e6
VANDER MOLEN <graff2k>
2023-04-12 04:03:39 -0500

    add query section

--- resources/Dashboard/main.js
+++ resources/Dashboard/main.js
@@ -5,10 +5,14 @@ function open_win(){
     window.open("https://grafana.cld.dtvops.net/d/slFUv7yZz/ov-service-monitoring-live-metrics?orgId=1&refresh=1m")
     window.open("https://grafana.cld.dtvce.com/d/KuDViSHMk/customer-impact-dashboard?orgId=1&refresh=30s&from=now-6h&to=now&var-Platform=All&var-cdn=All&var-pod=All&var-strmt=All&var-an=All&var-groupby=cdn&var-esDatasourceFilter=RaptorES&var-esDatasource=RaptorES&var-int=1m&var-indexMask=sessions*%5Byyyy.MM.dd%5D")
     window.open("https://grafana.cld.dtvce.com/d/IRRePhVMk/dfw-cdn-raptor-dashboard?orgId=1&refresh=1m")
-    window.open("https://splunk-labc.dtvops.net/en-US/app/streaming/drmslg_status?earliest=-15m&latest=now")
-    window.open("https://splunk-labc.dtvops.net/en-US/app/search/dma_downnetworkcircuit_flapdrop")
     window.open("https://one.newrelic.com/launcher/dashboards.launcher/?platform[timeRange][duration]=10800000&pane=eyJuZXJkbGV0SWQiOiJkYXNoYm9hcmRzLmRldGFpbCIsImVudGl0eUd1aWQiOiJNVFU1TURVM05ueFdTVnA4UkVGVFNFSlBRVkpFZkRFMU5UUTVNVEkiLCJ1c2VEZWZhdWx0VGltZVJhbmdlIjpmYWxzZSwic2VsZWN0ZWRQYWdlIjoiTVRVNU1EVTNObnhXU1ZwOFJFRlRTRUpQUVZKRWZERTFOVFE1TVRJIiwiaXNUZW1wbGF0ZUVtcHR5IjpmYWxzZX0=&state=de8c4aa9-8aa0-309b-3c1c-5ec0931ec663")
     window.open("https://grafana.cld.dtvce.com/d/WxKk5-4Mz/quality?viewPanel=9&orgId=1&from=now-24h&to=now&var-domain=OV&var-esDatasourceFilter=RaptorES&var-esDatasource=RaptorES&var-Platform=All&var-Asset=%5B1380%5D%5B360%5D%5BFox%20News%20Channel%5D&var-Asset=%5B8249%5D%5B202%5D%5BCNN%5D&var-Asset=%5B1590%5D%5B356%5D%5BMSNBC%5D&var-Asset=%5B8244%5D%5B229%5D%5BHGTV%5D&var-Asset=%5B1964%5D%5B206%5D%5BESPN%5D&var-Asset=%5B1327%5D%5B242%5D%5BUSA%20Network%5D&var-Asset=%5B6212%5D%5B285%5D%5BInvestigation%20Discovery%5D&var-Asset=%5B6055%5D%5B289%5D%5BDisney%20Junior%20HD%5D&var-Asset=%5B3771%5D%5B312%5D%5BHallmark%20Channel%5D&var-Asset=%5B8278%5D%5B231%5D%5BFood%20Network%5D&var-Asset=%5B1557%5D%5B269%5D%5BHISTORY%5D&var-Asset=%5B8262%5D%5B280%5D%5BTLC%5D&var-Asset=%5B1323%5D%5B237%5D%5BBravo%5D&var-Asset=%5B8208%5D%5B248%5D%5BFX%5D&var-Asset=%5B8254%5D%5B247%5D%5BTBS%5D&var-Asset=%5B1382%5D%5B265%5D%5BA%26E%5D&var-Asset=%5B1966%5D%5B245%5D%5BTNT%5D&var-Asset=%5B8274%5D%5B299%5D%5BNickelodeon%20East%20HD%5D&var-Asset=%5B1593%5D%5B236%5D%5BE!%5D&var-Asset=%5B4211%5D%5B311%5D%5BFreeform%20HD%5D&var-cdn=All&var-pod=All&var-stream=Live&var-dai=All&var-int=1m&var-indexMask=sessions*%5Byyyy.MM.dd%5D&var-Query=&var-esFilter=&var-groupByField=Asset&var-groupby=an&var-domainFilter=NOT%20serviceDomain:(TVE%20OR%20DTVE%20OR%20NFLST)&refresh=1m")
     window.open("https://grafana.cld.dtvops.net/d/rzCQwDp7z/dtve-service-monitoring-live-streaming?orgId=1&refresh=30s")
     window.open("https://grafana.cld.dtvops.net/d/4tYP9ut7z/dtve-service-monitoring-vod-streaming?orgId=1&refresh=1m")
+    window,open("https://grafana.cld.dtvops.net/d/3uUxRuzWz/authn-combined-dashboard?orgId=1")
+}
+
+
+function open_wind(){
+    window.open("https://one.newrelic.com/dashboards/detail/MTU5MDU3NnxWSVp8REFTSEJPQVJEfGRhOjI3NTg0ODI?duration=21600000&state=b1959489-212a-d9b2-3129-997c24dbced0")
 }
--- resources/Query/resources/Query/resources/main.js
+++
@@ -1,7 +0,0 @@
-function openstuffa() {
-    window.open("resources/Query/resources/Query/auto.html")
-}
-
-function opentstuffcust(){
-    window.open("resources/Query/resources/Query/custom.html")
-}
